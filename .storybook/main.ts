16594b4cfe74c0c4dfef847a0ab87a886ebf1f1d
Victor Rodrigues <victor.rodrigues.carvalho.santos@gmail.com>
2025-01-27 18:15:46 +0000

    build: :arrow_up: upgrade version of storybook from 7 to 8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,15 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-styling',
+    '@chromatic-com/storybook',
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {
@@ -15,8 +18,11 @@ const config: StorybookConfig = {
       },
     },
   },
-  docs: {
-    autodocs: 'tag',
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
   },
 }
 export default config
